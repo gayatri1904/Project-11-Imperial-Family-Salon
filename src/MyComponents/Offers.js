5a98209f40c8134a543eb4498cee4012e1ed303e
DeEpRaJ <pagaredeepraj05@gmail.com>
2023-02-21 01:08:43 +0530

    fataka

--- src/MyComponents/Offers.js
+++ src/MyComponents/Offers.js
@@ -4,6 +4,7 @@ import offer1 from '../MyResources/offer.jpg';
 import { Carousel } from 'react-bootstrap';
 import { useState } from 'react';
 import '../MyResources/Offers.css';
+import ConfettiExplosion from 'react-confetti-explosion';
 
 export default function Offers() {
     const [index, setIndex] = useState(0);
@@ -11,13 +12,18 @@ export default function Offers() {
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
+  const [isExploding, setIsExploding] = React.useState(false);
+  const mouseOver=()=>{
+    setIsExploding(true)
+  }
   return (
-    <div>
+    <div onMouseOver={mouseOver}>
+      <center>{isExploding && <ConfettiExplosion width="500" duration={3000} particleCount={150}/>}</center>
       <MediaQuery maxDeviceWidth={600}>
       <h1 className='service-head'>Offers</h1>
       <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-      <img src={offer1} className='offer' alt='image1' style={{width:"90vw",height:"600px"}}></img>
+      <img src={offer1} className='offer' alt='image1' style={{width:"90vw",height:"600px"}} ></img>
       </Carousel.Item>
     </Carousel>
       </MediaQuery>
